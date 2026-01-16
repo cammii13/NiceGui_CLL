63d3f8eff6d648a98781c470d52c4b4fb9381821
cammii13 <gllusca@gbmpaulovi.edu.ec>
2026-01-16 14:54:56 -0500

    Calculadora ui
    Programa completo sumar,restar, multiplicar y dividir

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 from nicegui import ui
 
 # Funciones
+def sumar():
+        n1 = float(num1.value)
+        n2 = float(num2.value)
+        res = n1 + n2
+        resultado.set_text(f'Resultado: {res}')
 
+def restar():
+        n1 = float(num1.value)
+        n2 = float(num2.value)
+        res = n1 - n2
+        resultado.set_text(f'Resultado: {res}')
+
+def multiplicar():
+        n1 = float(num1.value)
+        n2 = float(num2.value)
+        res = n1 * n2
+        resultado.set_text(f'Resultado: {res}')
+
+def dividir():
+        n1 = float(num1.value)
+        n2 = float(num2.value)
+        if n2 != 0:
+            res = n1 / n2
+            resultado.set_text(f'Resultado: {res}')
+        else:
+            resultado.set_text('Error: División por cero')  
 
 # Estructuras de la interfaz de usuario
 with ui.card().classes('w-60 m-auto p-4'):
-    ui.label('Hola, Mundo').classes("font-sans"):
+    ui.label('Calculadora').classes("font-sans")
+    num1 = ui.input('Número 1').classes('w-full')
+    num2 = ui.input('Número 2').classes('w-full')
+    bt_sumar = ui.button('Sumar').classes('w-full bg-blue-500 text-white').on('click', sumar)
+    bt_restar = ui.button('Restar').classes('w-full bg-blue-500 text-white').on('click', restar)
+    bt_multiplicar = ui.button('Multiplicar').classes('w-full bg-blue-500 text-white').on('click', multiplicar)
+    bt_dividir = ui.button('Dividir').classes('w-full bg-blue-500 text-white').on('click', dividir)
+    resultado = ui.label('Resultado: ').classes('w-full text-center mt-4')
+
 
 
 ui.run()
